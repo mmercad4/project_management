29133652c04f1336f32a307fd1d3492cc33e7524
Marco Mercado <marco@stonepawtraining.com>
2023-10-25 08:00:04 -0400

    Added ProjectSidebar Component

---
+++ src/components/ProjectSideBar.jsx
@@ -0,0 +1,13 @@
+const ProjectSidebar = () => {
+  return (
+    <aside>
+      <h2> Your Projects</h2>
+      <div>
+        <button>Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+};
+
+export { ProjectSidebar };

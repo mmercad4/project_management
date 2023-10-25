66202d26b7ce58474eae23fac53df41b4a76402f
Marco Mercado <marco@stonepawtraining.com>
2023-10-25 08:43:07 -0400

    Added state to dynamically render project state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 import { ProjectSideBar } from "./components/ProjectSideBar";
 import { NewProject } from "./components/NewProject";
 import { NoProjectSelected } from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar />
-      <NoProjectSelected />
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }

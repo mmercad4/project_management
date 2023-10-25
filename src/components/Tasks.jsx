9a3e5b8169870f1f92d7cc7a5090f1515a49e714
Marco Mercado <marco@stonepawtraining.com>
2023-10-25 12:44:51 -0400

    Added NewTask and Tasks components

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,17 @@
+import { NewTask } from "./NewTask";
+
+const Tasks = () => {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      NEW TASKS
+      <NewTask />
+      <p className="text-stone-800 my-4">
+        This project does not have any tasks yet.
+      </p>
+      <ul></ul>
+    </section>
+  );
+};
+
+export { Tasks };

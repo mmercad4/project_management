9e4febe793fc5628c09a73201de18ecad2286896
Marco Mercado <marco@stonepawtraining.com>
2023-10-25 08:07:12 -0400

    Added ProjectSideBar styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { ProjectSidebar } from "./components/ProjectSideBar";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <ProjectSidebar />
-    </>
+    </main>
   );
 }
 
